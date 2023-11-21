28a0ddb5f91868c90cebe05911a01652c4d2d971
Ryan Smith <ryan.smith@cityfibre.com>
2023-11-21 13:32:59 +0000

    add DB container

---
+++ app/Models/Beer.cs
@@ -0,0 +1,16 @@
+namespace app.Models;
+
+
+public class Beer
+{
+
+    public int Id { get; set; }
+    public string? BeerName { get; set; }
+    public string? BeerType { get; set; }
+
+    public Beer()
+    {
+
+    }
+
+}
